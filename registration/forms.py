f40a43549f876a230f0e885aac36ab9222878dcc
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-05 18:49:21 +0800

    Added Email Duplicate Validation on Registration

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Length, EqualTo
+from registration.models import User
 
 class RegistrationForm(FlaskForm):
   first_name = StringField('First Name', validators = [DataRequired(), Length(min = 1, max = 32)])
@@ -9,3 +10,9 @@ class RegistrationForm(FlaskForm):
   password = PasswordField('Password', validators = [DataRequired(), Length(min = 6, max = 32), EqualTo('confirm_password', message = 'Passwords do not match.')])
   confirm_password = PasswordField('Confirm Password')
   submit = SubmitField('Register')
+
+  def validate_email(self, email):
+    user = User.query.filter_by(email = email.data).first()
+    
+    if user is not None:
+      raise ValidationError('Email already exists.')
