9fd6b4713a7cc9bf3b680d7fc64f2e140a9743d8
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-05 19:15:03 +0800

    Added Email Format Validation in Registration

--- registration/forms.py
+++ registration/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Length, EqualTo, Email
 from registration.models import User
 
 class RegistrationForm(FlaskForm):
   first_name = StringField('First Name', validators = [DataRequired(), Length(min = 1, max = 32)])
   last_name = StringField('Last Name', validators = [DataRequired(), Length(min = 1, max = 32)])
-  email = StringField('Email', validators = [DataRequired(), Length(min = 5, max = 120)])
+  email = StringField('Email', validators = [Email(), DataRequired(), Length(min = 5, max = 120)])
   password = PasswordField('Password', validators = [DataRequired(), Length(min = 6, max = 32), EqualTo('confirm_password', message = 'Passwords do not match.')])
   confirm_password = PasswordField('Confirm Password')
   submit = SubmitField('Register')
