bcc1721433ef25f3837154bdacc32bb8c4fc0b62
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-05 18:48:52 +0800

    Added Login Required

--- artikulo/__init__.py
+++ artikulo/__init__.py
@@ -11,6 +11,7 @@ migrate = Migrate(app, db)
 
 from flask_login import LoginManager
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 from login.models import load_user
 
 from artikulo import routes
--- artikulo/artikulo.py
+++ artikulo/artikulo.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from artikulo import app
 
+from flask_login import login_required
+
 class Home:
+	@login_required
 	def index(self):
 		return render_template('home/index.html', title = 'Home')
