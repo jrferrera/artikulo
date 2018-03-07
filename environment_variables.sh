3686be3561faa7431610217655d86d74b64ca803
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-07 19:53:48 +0800

    Added Mailer Environment Variables

--- environment_variables.sh
+++ environment_variables.sh
@@ -3,3 +3,9 @@ export FLASK_APP=artikulo/artikulo.py
 export FLASK_DEBUG=true
 
 export SECRET_KEY=secret_key
+
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=username
+export MAIL_PASSWORD=password
