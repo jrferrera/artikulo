ead7dd9ac5b0f49dd56cccf7d0536212840e4410
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-07 15:11:41 +0800

    Added Show Profile by Username

--- profile/profile.py
+++ profile/profile.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint, render_template
 from flask_login import current_user, login_required
+from registration.models import User
 
 profile = Blueprint('profile', __name__, template_folder = 'templates')
 
 class Profile:
 	@login_required
-	def show(self):
-		return render_template('profiles/show.html', title = 'Profile')
+	def show(self, username):
+		user = User.query.filter_by(username = username).first_or_404()
+		return render_template('profiles/show.html', title = 'Profile', user = user)
--- profile/routes.py
+++ profile/routes.py
@@ -1,4 +1,4 @@
 from artikulo import app
 from profile.profile import Profile
 
-app.add_url_rule('/view_profile', 'view_profile', Profile().show, methods = ['GET'])
+app.add_url_rule('/profile/<username>', 'show_profile', Profile().show, methods = ['GET'])
