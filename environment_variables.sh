bdca277393be526d743393dd962d300da57715c3
Jefferson Ferrera <jrferrera17@gmail.com>
2018-03-08 19:01:37 +0800

    Updated Environment Variables

--- environment_variables.sh
+++ environment_variables.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export FLASK_APP=artikulo/artikulo.py
 export FLASK_DEBUG=true
-
 export SECRET_KEY=secret_key
-
+export DATABASE_URI=mysql://root:root@127.0.0.1:3306/artikulo_development
 export MAIL_SERVER=smtp.googlemail.com
 export MAIL_PORT=587
 export MAIL_USE_TLS=1
